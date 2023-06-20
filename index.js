23f6dfe4bbc8669d542818ff9cb02468a62e33bb
luisfelipe-tec <luisfelipe.tecnologias@gmail.com>
2023-06-19 23:41:34 -0300

    adicionado funções de busca

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('olá mundo');
+import fs from 'fs';
+import chalk from "chalk"; 
+
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'))
+}
+
+function pegaArquivo(caminhoArquivo){
+
+    const encoding = 'utf-8'
+    fs.readFile(caminhoArquivo, encoding, (erro, texto) => {
+
+        if(erro){
+            trataErro(erro);
+        }
+        console.log(chalk.green(texto))
+    });
+}
+ 
+pegaArquivo('./arquivos/texto.md ')
